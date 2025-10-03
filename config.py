9d7050ad25b6b1e1f896c29678d747d3ccae375e
Pablo Recalde <60885435+deltareflo@users.noreply.github.com>
2025-10-03 07:46:39 -0300

    Update config.py otro correo para envio de reseteo

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ class DeveloperConfig():
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 3
-    MAIL_SERVER= 'smtp.googlemail.com'
+    MAIL_SERVER= 'smtp.gmail.com'
     MAIL_PORT = 587
-    MAIL_USERNAME = 'mapadetalentos@aikumby.com'
+    MAIL_USERNAME = 'aikumby.vocacional@gmail.com'
     MAIL_PASSWORD = environ.get('EMAIL_HOST_PASSWORD')
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
