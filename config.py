bd6a5af5ac531b6f2ab8c1ad9ba298975e0627c6
Pablo Recalde <60885435+deltareflo@users.noreply.github.com>
2025-10-17 10:51:48 -0300

    Update config.py - se cambia de sqlite a posgres de neon

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class DeveloperConfig():
     FLASK_DEBUG = environ.get('FLASK_ENV')
     PWD = path.abspath(curdir)
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
+    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_kS5Obash2Lnf@ep-tiny-morning-advu7ex4-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 3
     MAIL_SERVER= 'smtp.gmail.com'
